3529f68b07362e01cc7504d3adfea0b8f737a4a6
TaKo86 <warneranash@gmail.com>
2026-09-22 23:30:57 +1200

    converyed to typscript with react using Vite

---
+++ my-app/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  return (
+    <main className="site-shell">
+      <iframe
+        className="site-frame"
+        src="/wbc-site.html"
+        title="WBC Muay Thai New Zealand"
+      />
+    </main>
+  )
+}
+
+export default App
